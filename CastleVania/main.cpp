1d90e1f15943090104acd05b8cda3b4ab6c0ff0d
Vuclover_0410 <123lnhvu@gmail.com>
2019-10-14 13:23:18 +0700

    ..
    first created

---
+++ CastleVania/main.cpp
@@ -0,0 +1,11 @@
+#include <d3d9.h>
+#include <Windows.h>
+int WINAPI WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpStrCmdLine,
+	int nShowCmd)
+{
+	MessageBox(0, "Welcome to Game Programming!", "Hello DirectX", MB_OK);
+	return 0;
+}
